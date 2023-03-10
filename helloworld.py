d0245181c3e4522b162f286bd01fd73c7e4b457b
Tejas <tejas.patiltp2802@gmail.com>
2023-03-10 15:32:02 +0530

    Python Practice

---
+++ helloworld.py
@@ -0,0 +1,59 @@
+person_first = "Tony"
+person_last = "Stark"
+age = 51
+print(person_first + " is a genious")
+#name = input("Enter your name")
+#print(name)
+#superhero_name = input("tejas enter your superhero name")
+#print(superhero_name + "is tejas's superhero")
+
+first = input("Your first number:")
+second = input("Your secoond number:")
+sum = int(first) + int(second)
+print(sum)
+name = "Tony Stark"
+print(name.replace("T", "M"))
+print(name.find("S"))
+
+print(name.upper)
+print(name.lower)
+
+print("S" in name)
+
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)  #to print the value with decimal we use the / operator
+print(5 // 2) #to remove value after the decimal we use the // operator
+print(5 % 2)  #it prints the remainder in the operation when we divide 5 / 2 the remainder we get is 1
+print(5 ** 2) #to get the product in the power method eg. sqaure, cube, etc
+
+
+i = 5
+i = i + 2
+i += 2
+i -= 2
+i *= 2
+
+
+result = 2 + 3 * 5
+print(result) # * > +
+
+
+print(3>2)
+print(3<2)
+print(3<=2)
+print(3>=2)
+print(3 == 2)
+
+
+print(3 != 2)
+print(3 != 3)
+
+
+#or
+print(2 > 1 or 2 > 3)
+print (2 > 1 and 2 > 3)
+print(not 3 > 2 )
+
+
